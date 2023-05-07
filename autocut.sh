1ae4c3659e7dfe48306ea123f4815fd5ab18c738
Eberhard Beilharz <eb1@sil.org>
2023-05-07 17:31:26 +0200

    Remove debug code

--- autocut.sh
+++ autocut.sh
@@ -6,8 +6,7 @@ cd "$(dirname "$0")"
 git pull origin main
 if [ ! -f last-install.sha1 ] || [[ "$(cat last-install.sha1)" != "$(git rev-parse HEAD)" ]]; then
     echo "Updating and installing required dependencies"
-    #./install.sh
+    ./install.sh
     git rev-parse HEAD > last-install.sha1
 fi
-exit 0
 ./autocut.py "$@"
