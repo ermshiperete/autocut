96550e5a5df3afd1f53c80ec87017182f0cc9d54
Eberhard Beilharz <eb1@sil.org>
2025-05-25 20:54:40 +0200

    Remove another wait for enter

--- autocut.py
+++ autocut.py
@@ -494,7 +494,6 @@ def process_audio(input_file, audio_file, services, use_start_time):
             if not args.no_upload:
                 # If we can't find intro we upload the full temp file to the website
                 upload_to_website(config, audio_file, info['year'])
-            input('Press Enter...')
             exit(1)
 
     segments = detect_detailed_segments(audio_file, startMilliseconds)
